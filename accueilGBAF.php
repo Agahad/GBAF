70d901df60ac188c353499824fb0b1728dd19a04
Alexandre Obert <aclhobert@gmail.com>
2021-07-08 18:34:35 +0200

    MAJ CSS avec body et page connexion

---
+++ accueilGBAF.php
@@ -0,0 +1,12 @@
+<!-- Intégration d'un suivi de session-->
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="styleGBAF.css"/> 
+		<title>GBAF</title>
+	</head>
+
+<?php include "header.php"
+?>
