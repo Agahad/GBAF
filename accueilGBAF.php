9c888fc0003286eea59d01b1a3aa64abd784c5dd
Alexandre Obert <aclhobert@gmail.com>
2021-07-08 19:22:03 +0200

    code HTML v1 accueil

--- accueilGBAF.php
+++ accueilGBAF.php
@@ -8,5 +8,28 @@
 		<title>GBAF</title>
 	</head>
 
-<?php include "header.php"
-?>
+<div id="accueil">
+	<?php include "header.php"
+	?>
+	<body>
+		<div id="Section_Presentation">
+			<p><h1>Le Groupement Banque Assurance Français (GBAF)</h1></p>
+			<p>Le GBAF est le représentant de la profession bancaire et des assureurs sur tous
+		les axes de la réglementation financière française. Sa mission est de promouvoir
+		l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des
+		pouvoirs publics.</p>
+			<p>Ce site vous permet de mieux connaître et faire mieux connaître les partenaires et acteurs du secteur bancaire. Vous pourrez ici donner votre avis sur les produits et services bancaires et financiers et ainsi accroître vos connaissances et les partager.</p>
+			<p><img src="images/illustrationaccueil.jpeg" width:1000 heigth:150></p>
+		</div>
+		<div id="Section_Acteurs">
+			<p><h2>Présentation des acteurs</h2></p>
+			<p>Retrouvez ci-dessous la liste des acteurs identifiés par la GBAF. Vous pouvez obtenir davantage d'informations sur chacun d'entre eux et bien entendu partagez vos propres commentaires!</p>
+			<!--Appel BDD acteur-->
+			<!--boucle while pour afficher liste des acteurs-->
+			 
+		</div>
+
+	<?php include "footerGBAF.php"
+	?>
+</div>
+</html>
