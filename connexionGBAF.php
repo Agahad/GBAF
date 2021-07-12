be40357560bc2158977990cbd00d63841e346487
Alexandre Obert <aclhobert@gmail.com>
2021-07-12 16:43:41 +0200

    MAJ page connexion selon cahier des charges : ajout header et footer et champs sur toute largeur

--- connexionGBAF.php
+++ connexionGBAF.php
@@ -7,23 +7,27 @@
 		<link rel="stylesheet" href="styleGBAF.css"/> 
 		<title>GBAF</title>
 	</head>
+	<?php include "header.php"
+	?>
 
-
-<div id="page_connexion">
-<body>
-	<div id="headerconnexion">
-	<!-- Insertion du logo GBAF-->
-		<div id="logoGBAF">
-		<p><img src="images/logo GBAF.png" alt="logo GBAF" titre="logo GBAF" width=100> 
-		</div>
-		Merci de renseigner votre login et votre mot de passe pour accéder au service GBAF </p>
-	</div>
+	<div id="page_connexion">
+	<body>
+		<!-- En-tête du formulaire-->
+		<div id="headerconnexion">
 		
-	<!-- Insertion formulaire de connexio-->
-	<form action="accueilGBAF.php" method="POST">
-		<p><label> Login : <input type="text" name="login"/></label></p>
-		<p><label> Mot de passe : <input type="password" name="mdp"/></label></p>
-		<p><input type="submit" value="Se connecter"></p>
-	</form>
-	<div id="lienmdpoubli"><p><a href="mailto:admin@gbaf.com">Mot de passe oublié?</a></p></div><!--envoi mail automatique avec objet et texte ?-->
-</body>
+			Merci de renseigner votre login et votre mot de passe pour accéder au service GBAF </p>
+		</div>
+			
+		<!-- Formulaire de connexion-->
+		<form action="accueilGBAF.php" method="POST">
+			<p><label> Login : <input type="text" name="login"/></label></p>
+			<p><label> Mot de passe : <input type="password" name="mdp"/></label></p>
+			<p><input type="submit" value="Se connecter"></p>
+		</form>
+		<div id="lienmdpoubli"><p><a href="mailto:admin@gbaf.com">Mot de passe oublié?</a></p></div><!--envoi mail automatique avec objet et texte ?-->
+	</body>
+
+	<?php include "footerGBAF.php"
+	?>
+
+</html>
