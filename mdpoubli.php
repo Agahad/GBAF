883e229b4391b86c6edcf6facb488539e962f38c
Alexandre Obert <aclhobert@gmail.com>
2021-07-17 10:39:06 +0200

    mise à jour formulaire partie 1 ok avec vérif login, QS et Réponse_QS

--- mdpoubli.php
+++ mdpoubli.php
@@ -25,9 +25,9 @@
 					<label for="login"> Login </br> <input type="text" name="login" required /></label></br>
 					<label for="QS">Votre question secrète </br> </label>
 					<select name="QS" id="QS">
-						<option value="NJFVM">Nom de jeune fille de votre mère</option>
-						<option value="NAC">Nom de votre animal de compagnie</option>
-						<option value="POP">Pour vous, c'est quoi la POP culture?</option>
+						<option value="Nom de jeune fille de votre mère">Nom de jeune fille de votre mère</option>
+						<option value=">Nom de votre animal de compagnie">Nom de votre animal de compagnie</option>
+						<option value="Pour vous, c'est quoi la POP culture?">Pour vous, c'est quoi la POP culture?</option>
 					</select></br>
 					<label for="Reponse_QS"> Réponse à la question secrète </br> <input type="text" name="Reponse_QS" required /></label></br>
 					<input type="submit" value="Envoyer">
@@ -39,19 +39,43 @@
 			else
 			{
 				//On prépare les données relatives à la table account avec la possibilité de filter sur le username//
-				$login= $_POST['login'];
 				$req_mdp = $bdd->prepare('SELECT * FROM account WHERE username= :login');
-				$req_mdp->execute(array('login' => $login));
+				$req_mdp->execute(array('login' => $_POST['login']));
 				$resultat_mdp = $req_mdp->fetch();
 
+			//si un des critères est faux, on affiche message d'erreur//
+				if(!$resultat_mdp OR $_POST['QS']!==$resultat_mdp['question'] OR $_POST['Reponse_QS']!==$resultat_mdp['reponse'])//
+				{
+
+			?>
+					<form  method="post" action="mdpoubli.php"><!-- Vérif Login et Question Secrète-->
+						<label for="login"> Login </br> <input type="text" name="login" required /></label></br>
+						<label for="QS">Votre question secrète </br> </label>
+						<select name="QS" id="QS">
+							<option value="Nom de jeune fille de votre mère">Nom de jeune fille de votre mère</option>
+							<option value="Nom de votre animal de compagnie">Nom de votre animal de compagnie</option>
+							<option value="Pour vous, c'est quoi la POP culture?">Pour vous, c'est quoi la POP culture?</option>
+						</select></br>
+						<label for="Reponse_QS"> Réponse à la question secrète </br> <input type="text" name="Reponse_QS" required /></label></br>
+						<input type="submit" value="Envoyer">
+					</form>
+					<div class="message erreur"><p>Votre login et/ou votre réponse sont erronnés : merci de vérifier vos informations.</p></div>
+				<?php
+				}
+				else
+				{ 
+				?> <p>C'est bon mec</p>
+				<?php
+				}
 				?>
-					<!--si un des critères est faux, on affiche message d'erreur-->
+					
 					<!--sinon on affiche formulaire mot de passe sous condtions :-->
 						<!-- si le formulaire est vide, on affiche le formulaire vierge-->
 						<!-- sinonsi le mot de passe est différent du vérif mot de passe, on affiche un message d'erreur-->
 						<!-- sinon on affiche message de succès et modification mot de passe-->
-					<form  method="post" action="mdpoubli.php"><!-- Vérif Login et Question Secrète-->
-						<label for="login"> Login </br> <input type="text" name="login" required /></label></br>
+
+					<!--<form  method="post" action="mdpoubli.php"Vérif Login et Question Secrète-->
+					<!--	<label for="login"> Login </br> <input type="text" name="login" required /></label></br>
 						<label for="QS">Votre question secrète </br> </label>
 						<select name="QS" id="QS">
 							<option value="NJFVM">Nom de jeune fille de votre mère</option>
@@ -63,7 +87,7 @@
 						<label for="verifpassword"> Vérification Mot de Passe </br> <input type="password" name="verifpassword" placeholder="Resaisissez votre mot de passe" required /></label></br>
 						<input type="submit" value="Envoyer">
 				
-					</form>
+					</form>-->
 			<?php } ?>
 		<a href="connexionGBAF.php">Retour à la page connexion</a>	
 		</section>
