e937fb74d0fe82aa631e1a337d43b0d5f1e47d9e
Alexandre Obert <aclhobert@gmail.com>
2021-07-21 10:46:58 +0200

    ajout newpost au sein de la page via bouton

--- pageacteur.php
+++ pageacteur.php
@@ -1,5 +1,7 @@
 <?php
+//On démarre la session//
 session_start();
+//si pas de session => envoi message d'erreur et lien vers accueil//
 if (!isset($_SESSION['Nom'])) 
 {
 include "headervierge.php";
@@ -7,7 +9,7 @@ include "headervierge.php";
 <p><a href="homepageGBAF.php">Retour page d'accueil</a></p>
 <?php
 }
-//sinon on affiche la page de connexion
+//sinon on affiche la page//
 else
 {
 ?>
@@ -44,23 +46,47 @@ else
 				<p><?php echo $reponse_acteur['description'] ?></p>
 			</article>
 
-			<div id="pageacteur_commentaires">
+			<article id="pageacteur_commentaires">
 			<!-- On sélectionne la table commentaire en fitrant sur l'id acteur-->
 
 				<div id="entete_commentaires">
+					<!-- on indique le nombre de commentaires sur la page acteur en cours-->
 					<p><strong>'Compteur post where post.id_acteur=$id_acteur' Commentaire(s)</strong></p>
 					<div id="entete_commentaires_reactions">
-						<p class="bouton"><a href="newpostGBAF.php?acteur=<?php echo $id_acteur ?>">Ajouter un commentaire</a></p>
+						<!--on insère un bouton permettant l'ajout d'un nouveau commentaire sur la page en cours-->
+						<form method="post" action="pageacteur.php?acteur=<?php echo $id_acteur?>" class="bouton">
+							<input type="text" value="1" name="addpost" hidden>
+							<input type="submit" value ="Ajouter un commentaire">
+						</form>
+						<!--On ajoute nombre de like/bouton pour ajouter un like/nombre de dislike/bouton pour ajouter un dislike voir cahier pour code-->
 						<p>XX <a href="ajout 1 like"><img src="images/poucehaut.png" title="j'aime"></a> <a href="ajout 1 like"><img class="poucebas" src="images/poucebas.png" title="je n'aime plus"></a></p>
 					</div>
 				</div>
-				<div id="cartouche_commentaires">
+				<div class="newpost">
+					<?php
+					if(isset($_POST['addpost']))
+					{
+						?><form method="post" action="pageacteur.php?acteur=$id_acteur">
+							<label for="newpost">Votre Commentaire :</label><br />
+							<textarea rows="5" cols="80" id="post" name="newpost" required /></textarea><br />
+							<input type="submit" value="Partager votre commentaire">
+						</form>	
+						<?php			
+					}
+					else
+					{
+					}
+					?>
+				</div>
+
+				<div class="cartouche_commentaires">
+					
 					<!--boucle while-->
 					<p>Echo Prenom</p>
 					<p>Echo date</p>
 					<p> Echo texte commentaire</p>
 				</div>
-			</div>
+			</article>
 		</section>
 		<?php include "footerGBAF.php" ?>
 		
